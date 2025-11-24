1ef1e33eee23355a9667a2f21b418a593739a544
Manos Ragiadakos <manos.ragiadakos@gmail.com>
2025-11-24 22:04:43 +0200

    refactor name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="automatic-spoon-client-sync",
+    name="automatic_spoon_client_sync",
     version="0.1",
     license="MIT",
     author="Manos Ragiadakos",
     author_email="",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    url="https://github.com/rm4n0s/automatic-spoon-client-sync",
+    url="https://github.com/rm4n0s/automatic_spoon_client_sync",
     keywords="http client automatic-spoon",
     install_requires=["httpx"],
     include_package_data=True,
